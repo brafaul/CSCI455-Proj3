1f430dc0017132c14012e037538a449040eea51b
brafaul <31667642+brafaul@users.noreply.github.com>
2019-04-18 21:20:53 -0500

    Added some contents

--- proj3.py
+++ proj3.py
@@ -1,14 +1,20 @@
 import math
 
+#Stores each class
 class Group:
+    #stores each do in the class
     docList = []
+    #stores the centroid of the class
     centroid = []
+    #keeps track of how many docs were in the doc
     count = 0
+    #stores the sum of all scores to make computing the centroids easier
     totalScores = []
     def __init__(self, className):
         self.docList = []
         self.centroid = []
         self.className = className
+    #adds a new doc to the class
     def addItem(self, doc):
         self.docList.append(doc)
         self.count += 1
@@ -17,47 +23,50 @@ class Group:
                 self.totalScores = self.totalScores + [0]
         for i in range(len(doc.scores)):
             self.totalScores[i] += doc.scores[i]
-
+    #calculates the centroid of each class
     def getCentroid(self):
         for i in self.docList[0].scores:
             self.centroid = self.centroid + [0]
         for i in range (len(self.centroid)):
             self.centroid[i] = (self.totalScores[i]/self.count)
 
+#stores the name and scores of each document along with what class it is in
 class Document:
     def __init__(self, name, scores, className):
         self.name = name
         self.scores = scores
         self.className = className
-
+#takes a document a group list as parameters and figures out which centroid the doc is closest to
 def classify(doc, groupList):
+    #stores the distance from each centroid
     distances = []
+    #intialized to an insanely large number for comparison purpose
     lowestDistance = 100000000
     currClass = -1;
     for i in range(len(groupList)):
         tempSum = 0
         distances.append(0)
-        #print(groupList[i].centroid)
+        #calculates the distance from each centoid
         for j in range(len(groupList[i].centroid)):
             tempSum += (groupList[i].centroid[j] - doc.scores[j])**2
-            #print(tempSum)
         distances[i] = math.sqrt(tempSum)
-        #print(str(distances[i]) + " " +groupList[i].className)
-    #for i in :
-        #print(doc.name + " " + str(i))
+    #Finds which centroid has the shortest distance from the doc
     for i in range(len(groupList)):
         if distances[i] < lowestDistance:
             lowestDistance = distances[i]
             currClass = i
-    #print(lowestDistance)
     print(doc.name + " is of class " + groupList[currClass].className)
 
 print("Enter in the name of the document file")
+#reads in the name of the file to get the docs from
 docFileName = input()
+#stores all the docs
 allDocs = []
+#stores all of the classes
 groupList = []
 docFile = open(docFileName, "r")
 lineOne = docFile.readline()
+#reads in the files and creates doc classes for each doc in the file
 while(lineOne):
     lineTwo = docFile.readline()
     className = docFile.readline()
@@ -69,44 +78,41 @@ docFile.close()
 print("Please enter in the name of the test file")
 testFileName = input()
 testFile = open(testFileName, "r")
+#reads in the name of all the test docs and splits them into an array
 temp = testFile.read()
 testDocs = temp.split()
-#for i in testFiles:
-#    print(i)
+testFile.close()
 print(len(allDocs))
+#Adds each file into the approiate class 
 for i in range(len(allDocs)):
+    #Checks if a doc is a test doc
     inTest = False
     tempName = allDocs[i].name[:-1]
     for k in testDocs:
         if(tempName == k):
             inTest = True
-            #print(tempName)
     if inTest == False:
+        #creates and add new files to existing classes
         found = False
-        #print(allDocs[i].name)
         for j in range (len(groupList)):
             if groupList[j].className == allDocs[i].className:
                 groupList[j].addItem((allDocs[i]))
                 found = True
-                #print("found")
         if found == False:
-            #print("not found")
             groupList.append(Group(allDocs[i].className))
             groupList[-1].addItem((allDocs[i]))
 for i in range(len(groupList)):
+    #Just removes the newline char from the end of each class name
     groupList[i].className = groupList[i].className[:-1]
-    print(groupList[i].className + " " + str(len(groupList[i].docList)))
+    #print(groupList[i].className + " " + str(len(groupList[i].docList)))
     groupList[i].getCentroid()
-    #print(groupList[i].centroid)
+#classify test docs
 for i in testDocs:
     for j in allDocs:
         tempName = j.name[:-1]
         if i == tempName:
             classify(j,groupList)
-for i in groupList:
-    print(i.className)
-    print(i.count)
-    #print(len(i.docList))
-    #for j in i.centroid:
-    #     print(j)
-    print()
+#for i in groupList:
+#    print(i.className)
+#    print(i.count)
+#    print()
